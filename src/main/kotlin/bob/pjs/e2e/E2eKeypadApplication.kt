e2148a72cbc5a8acdb3db6761ce2b5aef9bb929d
Jeongsik Park <ori@khu.ac.kr>
2024-07-20 22:33:48 +0900

    [edit] update files

--- src/main/kotlin/bob/pjs/e2e/E2eKeypadApplication.kt
+++ src/main/kotlin/bob/pjs/e2e/E2eKeypadApplication.kt
@@ -1,4 +1,12 @@
 package bob.pjs.e2e
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
 class E2eKeypadApplication {
+}
+
+fun main(args: Array<String>) {
+    runApplication<E2eKeypadApplication>(*args)
 }
--- src/main/kotlin/bob/pjs/e2e/controller/TestController.kt
+++ src/main/kotlin/bob/pjs/e2e/controller/TestController.kt
@@ -1,4 +1,12 @@
 package bob.pjs.e2e.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class TestController {
+    @GetMapping("/test/a")
+    fun testA(): String {
+        return "Hello Spring!"
+    }
 }
